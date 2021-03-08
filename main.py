9b2743d8f61a94fb488ad716ef4342a44bba45b3
thomas <73606143+tthomxs@users.noreply.github.com>
2021-03-08 19:27:57 +0000

    Created main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from discord_webhook import DiscordWebhook, DiscordEmbed
+import time
+
+def spam():
+  webhook = DiscordWebhook(url='WEBHOOK_URL', content='@everyone')
+
+  ## replace WEBHOOK_URL with your webhook url
+
+  response = webhook.execute()
+
+while True:
+  spam()
+  time.sleep(1) ## replace 1 with number of seconds you want to wait
